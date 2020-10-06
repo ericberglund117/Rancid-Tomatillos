a5db7425b5ded22c6620fbe7e0cae1a2da061933
Eric Berglund <ericberglund117@gmail.com>
2020-10-06 15:44:10 -0600

    Create fetch api file

---
+++ src/Api.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const apiFetcher = {
+  getAllData() {
+    let allMoviesData = fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")  
+  }
+}
+
+
+export default Api;
