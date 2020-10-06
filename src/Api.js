f33fe59028f63bb80a868e29d82aa8473aa2ac79
Eric Berglund <ericberglund117@gmail.com>
2020-10-06 16:35:55 -0600

    Create App component to display movies information

--- src/Api.js
+++ src/Api.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
-const apiFetcher = {
-  getAllData() {
-    let allMoviesData = fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")  
-  }
-}
-
-
-export default Api;
+// import React from 'react';
+//
+// // const apiFetcher = {
+// //   getAllData() {
+// //     let allMoviesData = fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+// //   }
+// // }
+//
+//
+// export default apiFetcher;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Api from './Api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+    .then(response => response.json())
+    .then(data => this.setState({ movies: data.movies }));
+  }
+
+  separateMovies() {
+    const moviesToDisplay = this.state.movies.map((movie, index)=> {
+      return (
+        <section>
+          <img src={movie.poster_path}></img>
+          <h2 key={index}>{movie.title}</h2>
+          <h3 key={index}>{movie.average_rating}</h3>
+          <h3 key={index}>{movie.release_date}</h3>
+        </section>
+      )
+    })
+    return (
+      <section className="movies-list">
+        <h2>{moviesToDisplay}</h2>
+      </section>
+    )
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>Rancid Tomatillos</h1>
+        <h2>{this.separateMovies()}</h2>
+      </section>
+    )
+  }
 }
 
 export default App;
