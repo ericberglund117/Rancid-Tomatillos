cc2241a477d14b24f657bdd6a3be79c86e9d403f
Eric Berglund <ericberglund117@gmail.com>
2020-10-08 16:52:12 -0600

    Refactor error handling

--- src/login/Login.js
+++ src/login/Login.js
@@ -7,7 +7,7 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
-      error: null
+      error: ''
     };
   }
 
@@ -17,9 +17,13 @@ class Login extends Component {
   }
 
   submitLogin = (event) => {
-    event.preventDefault();
     const newLogin = this.state
     this.getUser(newLogin)
+    this.clearInputs();
+  }
+
+  clearInputs = () => {
+    this.setState({email:'', password:''})
   }
 
   getUser = (login) => {
@@ -31,44 +35,45 @@ class Login extends Component {
       },
       body: JSON.stringify(login)
     })
-    .then(response => response.json())
-    .then(data => this.props.setUser(login))
-    .catch(error => this.setState({ error }));
+    .then(response => {
+      if (response.ok) {
+        this.setState({error: ''})
+        return response.json()
+      }})
+    .then(data => this.props.setUser(data.user))
+    .catch(error => this.setState({ error: 'Incorrect username or password' }));
   }
 
   render() {
     const { email, password, error } = this.state;
-
-    if (error) {
-      return <p>{'Username or password is incorrect'}</p>;
-    }
-
-    return (
-      <form>
-      <label htmlFor="email">
-        Email
-      </label>
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          value={this.state.email}
-          onChange={event => this.handleChange(event)}
-        />
-        <label htmlFor="password">
-          Password
+      return (
+        <form>
+        {!error ? '' : <p>{error}</p>}
+        <label htmlFor="email">
+          Email
         </label>
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={this.state.password}
-          onChange={event => this.handleChange(event)}
-        />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={this.state.email}
+            onChange={event => this.handleChange(event)}
+          />
+          <label htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={this.state.password}
+            onChange={event => this.handleChange(event)}
+          />
 
-        <button onClick={event => this.submitLogin(event)}>Submit</button>
-      </form>
-    )
+          <button type="button" onClick={event => this.submitLogin(event)}>Submit</button>
+        </form>
+      )
+    //}
   }
 }
 
