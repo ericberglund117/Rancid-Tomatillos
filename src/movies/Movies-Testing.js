1c4d692a37094cb6efaee899da70a1814ed62a1f
Michael Walker <Michael.e.walker.87@gmail.com>
2020-10-10 15:34:33 -0600

    Testing fomrating

--- src/movies/Movies-Testing.js
+++ src/movies/Movies-Testing.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, from 'react';
+import { render, screen } from '@testing-library/react'
 import Movies from './Movies.js';
-import {render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import {Router} from 'react-router-dom'
 
 describe("Movies", () => {
   it("Should render a movie", () => {
-    render(
-      <Movie
-        image=
-          <img
-            src='https://image.tmdb.org/t/p/original/
-            /aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg'
-            className='image-poster'
-          />
-        id={3}
-        title='Mulan'
-        rating={6}
-        date='2020-09-04'
-      />
-    )
-    expect(sreen.Movie.toHaveAttribute('src',
-      'https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg'))
+    render(<Movie
+            image=
+              <img
+                src='https://image.tmdb.org/t/p/original/
+                /aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg'
+                className='image-poster'
+              />
+            id={3}
+            title='Mulan'
+            rating={6}
+            date='2020-09-04'
+            />);
+    expect(screen.getByTest('Mulan')).toBeInTheDocument();
   })
+
 })
