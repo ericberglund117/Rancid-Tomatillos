2911127dcbbe9d49f07b8aa1a8c667ded89e9438
Eric Berglund <ericberglund117@gmail.com>
2020-10-11 15:53:36 -0600

    Refactor Movies.test to be asynchronous to test if movies are rendered

--- src/movies/Movies.js
+++ src/movies/Movies.js
@@ -16,29 +16,12 @@ export default class Movies extends Component {
     .catch(error => this.setState({ error, isLoading: false}));
   }
 
-
-  // displayMovieCards() {
-  //   return this.state.movies.map((movie, index) => {
-  //     return (
-  //       <article key={index} className='poster-card' id={movie.id}>
-  //         <img alt='image-poster' src={movie.poster_path}></img>
-  //         <section className='poster-card-text'>
-  //           <h2 className='title-poster'>{movie.title}</h2>
-  //           <h3 className='rating-poster'>Average Rating: {movie.average_rating}</h3>
-  //           <h3 className='release-date-poster'>Release Date: {movie.release_date}</h3>
-  //         </section>
-  //       </article>
-  //     )
-  //   })
-  // };
-
   render () {
     return (
-      <>
-        <section className="movies-list">
+        <section className="movies-list" title="movies-list">
           { this.state.movies.map((movie, index) => {
             return (
-              <article key={index} className='poster-card' id={movie.id}>
+              <article key={index} className='poster-card' title='poster-card' id={movie.id}>
                 <img alt='image-poster' src={movie.poster_path}></img>
                 <section className='poster-card-text'>
                   <h2 className='title-poster'>{movie.title}</h2>
@@ -46,10 +29,9 @@ export default class Movies extends Component {
                   <h3 className='release-date-poster'>Release Date: {movie.release_date}</h3>
                 </section>
               </article>)
-          })
-            }
+            })
+          }
         </section>
-      </>
     )
   }
 };
--- src/movies/Movies.test.js
+++ src/movies/Movies.test.js
@@ -8,43 +8,29 @@ jest.mock('../apiCalls.js');
 
 describe("Movies", () => {
   it('should render a movie', async () => {
-    getAllMovies.mockResolvedValueOnce([{
-      id: 524047,
-      poster_path: "https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg",
-      backdrop_path: "https://image.tmdb.org/t/p/original//juzEhsX92if2lJ2CSqKAI4RQswt.jpg",
-      title: "Money Plane",
-      average_rating: 9,
-      release_date: "2020-07-29"
-    }])
-
-    const { getByText } = render(<Movies  />);
-    screen.debug()
-    const moviesContainer = getByText("Movies");
-    screen.debug()
-    const movie = await waitFor(() => getByText('Money Plane'))
+    getAllMovies.mockResolvedValueOnce({ movies: [
+      {
+         id: 694919,
+         poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+         backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+         title: "Money Plane",
+         average_rating: 9,
+         release_date: "2020-09-29"
+       },
+    ]})
+    //render component
+    const { getByTitle, getByText, getByAltText } = render(<Movies  />);
+    // check that there is a container element on the page
+    const moviesContainer = getByTitle("movies-list");
+    const movieTitle = await waitFor(() => getByText('Money Plane'))
+    const movieDate = await waitFor(() => getByText('Release Date: 2020-09-29'))
+    const movieRating = await waitFor(() => getByText('Average Rating: 9'))
+    const moviePoster = await waitFor(() => getByAltText('image-poster'))
+    // check that there are movies on the page
     expect(moviesContainer).toBeInTheDocument();
-    expect(movie).toBeInTheDocument();
-
-
-      // screen.debug();
-      // expect(screen.getByText('Money Plane')).toBeInTheDocument();
-      // expect(screen.getByText("Average Rating: 9")).toBeInTheDocument();
-      // expect(screen.getByText("Release Date: 2020-07-29")).toBeInTheDocument();
-      // expect(screen.getByAltText("image-poster")).toBeInTheDocument();
+    expect(movieTitle).toBeInTheDocument();
+    expect(movieDate).toBeInTheDocument();
+    expect(movieRating).toBeInTheDocument();
+    expect(moviePoster).toBeInTheDocument();
     })
 })
-  // let container = null;
-  // beforeEach(() => {
-  //   container = document.createElement('section')
-  //   document.body.appendChild(container)
-  // })
-  //
-  // it("Should display movies section", () => {
-  //   const mockDisplayMovies = jest.fn();
-  //   act(() => {
-  //     render(<Movies />, container);
-  //   });
-  //   screen.debug();
-  //   expect(container).toBeInTheDocument();
-  //   expect(mockDisplayMovies).toHaveBeenCalled();
-  // })
