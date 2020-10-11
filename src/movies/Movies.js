2d373af903ce0b0105513704bf59f921a4da26c4
Michael Walker <Michael.e.walker.87@gmail.com>
2020-10-11 16:13:20 -0600

    Add a method to get the id for a movie

--- src/movies/Movies.js
+++ src/movies/Movies.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getAllMovies } from '../apiCalls'
 import "./Movies.css"
+import SingleMovie from "../single-movie/SingleMovie.js"
 
 export class Movies extends Component {
   constructor() {
@@ -10,27 +11,16 @@ export class Movies extends Component {
     };
   }
 
+  getMovieID(event) {
+    console.log(event.currentTarget.dataset.id)
+    return event.currentTarget.dataset.id
+  };
+
   componentDidMount () {
     getAllMovies()
     .then(data => this.setState({ movies: data.movies }))
     .catch(error => this.setState({ error, isLoading: false}));
   }
-
-
-  // moviesToDisplay () {
-  //   return this.state.movies.map((movie, index) => {
-  //     return (
-  //       <article key={index} className='poster-card' id={movie.id}>
-  //         <img className='image-poster' src={movie.poster_path}></img>
-  //         <section className='poster-card-text'>
-  //           <h2 className='title-poster'>{movie.title}</h2>
-  //           <h3 className='rating-poster'>Average Rating: {movie.average_rating}</h3>
-  //           <h3 className='release-date-poster'>Release Date: {movie.release_date}</h3>
-  //         </section>
-  //       </article>
-  //     )
-  //   })
-  // }
 
   render () {
     return (
@@ -38,7 +28,11 @@ export class Movies extends Component {
         {this.state.movies.map((movie, index) => {
           return (
             <article key={index} className='poster-card' id={movie.id}>
-              <img alt='image-poster' src={movie.poster_path}></img>
+              <img
+                alt='image-poster'
+                data-id={movie.id}
+                src={movie.poster_path}
+                onClick={this.getMovieID}></img>
               <section className='poster-card-text'>
                 <h2 className='title-poster'>{movie.title}</h2>
                 <h3 className='rating-poster'>Average Rating: {movie.average_rating}</h3>
