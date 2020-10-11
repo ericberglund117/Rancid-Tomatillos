b988035199bf3dccb3a10a3f8c31f665e618ef28
Michael Walker <Michael.e.walker.87@gmail.com>
2020-10-11 11:52:55 -0600

    Set up the API for the single movie

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,8 @@ export const getAllMovies = () => {
   return fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
   .then(response => response.json())
 };
+
+export const getSingleMovie = (idNumber) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${idNumber}`)
+  .then(response => response.json())
+};
