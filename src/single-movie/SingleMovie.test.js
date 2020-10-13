ecd280da17655dc1f033f2f06cd7853bc8a2a62b
Michael Walker <Michael.e.walker.87@gmail.com>
2020-10-12 21:16:29 -0600

    Found the bug for the single movie test

--- src/single-movie/SingleMovie.test.js
+++ src/single-movie/SingleMovie.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import Movies from './Movies.js';
+import Movies from '../movies/Movies.js';
+import SingleMovie from './SingleMovie'
 import '@testing-library/jest-dom'
 jest.mock('../apiCalls.js')
