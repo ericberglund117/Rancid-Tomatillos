fc7f3fd101289271eda09b3a85a7601059d29ccd
Michael Walker <Michael.e.walker.87@gmail.com>
2020-10-16 13:57:27 -0600

    Set up the if condtional in the userratings for delete

--- src/user-ratings/UserRatings.js
+++ src/user-ratings/UserRatings.js
@@ -60,46 +60,47 @@ export default class UserRatings extends Component {
       let userMovieRating = ratings.find(rating => {
         return parseInt(movieId) === rating.movie_id
       })
-      return userMovieRating.id
+      return userMovieRating
     }
 
   render() {
-    return (
-      {this.checkMovieRating(this.props.movieID)} 
-      ?
-        <form className="ratings-dropdown">
-          <label htmlFor="ratings">Select a movie rating option(1-lowest, 10-highest)</label>
-          <select id="movie-ratings"
-                  name="movie-ratings"
-                  defaultValue={this.state.selectedRating}
-                  onChange={this.changeValue}>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-            <option value={6}>6</option>
-            <option value={7}>7</option>
-            <option value={8}>8</option>
-            <option value={9}>9</option>
-            <option value={10}>10</option>
-          </select>
-          <button type="button"
-                  onClick={event => this.submitRating(event)}>Submit Rating</button>
-        </form>
-  
-      :
-      <section className='delete-rating'>
-        <label className='delete-lable'>
-          Already rated
-        </label>
-        <button 
-          type="button"
-          className='delete-button'
-          onClick={event => this.submitRating(event)}>
-          Delete Rating
-        </button>
-      </section> 
-    )
+    if(this.checkMovieRating(this.props.movieID)){
+      return (
+        <section className='delete-rating'>
+          <label className='delete-lable'>
+            Already rated
+          </label>
+          <button 
+            type="button"
+            className='delete-button'
+            onClick={event => this.submitRating(event)}>
+            Delete Rating
+          </button>
+        </section> 
+      )
+    } else {
+        return (
+            <form className="ratings-dropdown">
+              <label htmlFor="ratings">Select a movie rating option(1-lowest, 10-highest)</label>
+              <select id="movie-ratings"
+                      name="movie-ratings"
+                      defaultValue={this.state.selectedRating}
+                      onChange={this.changeValue}>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+                <option value={6}>6</option>
+                <option value={7}>7</option>
+                <option value={8}>8</option>
+                <option value={9}>9</option>
+                <option value={10}>10</option>
+              </select>
+              <button type="button"
+                      onClick={event => this.submitRating(event)}>Submit Rating</button>
+            </form>
+        )
+      }
   }
 }
