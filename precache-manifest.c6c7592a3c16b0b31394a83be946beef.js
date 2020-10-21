fca16f4ae4f8dcf12abec1a97bb2b8c9cc38a33e
Eric Berglund <ericberglund117@gmail.com>
2020-10-20 18:59:09 -0600

    Updates

---
+++ precache-manifest.c6c7592a3c16b0b31394a83be946beef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ef0c2ad01ff2c13fe79f5c29b8fb36",
+    "url": "/Rancid-Tomatillos/index.html"
+  },
+  {
+    "revision": "f02ed7ed1cd70a32b3ce",
+    "url": "/Rancid-Tomatillos/static/css/main.22e044d9.chunk.css"
+  },
+  {
+    "revision": "01a5b30ed95baf36bcd9",
+    "url": "/Rancid-Tomatillos/static/js/2.cb548bbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rancid-Tomatillos/static/js/2.cb548bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02ed7ed1cd70a32b3ce",
+    "url": "/Rancid-Tomatillos/static/js/main.6955d4c0.chunk.js"
+  },
+  {
+    "revision": "066c49850db5766b6e13",
+    "url": "/Rancid-Tomatillos/static/js/runtime-main.2455ce0b.js"
+  },
+  {
+    "revision": "f84560def5329d48ca7e208fd2ca93aa",
+    "url": "/Rancid-Tomatillos/static/media/cinema.f84560de.png"
+  },
+  {
+    "revision": "f89c729e9a3ef03ae368456f9c6c87af",
+    "url": "/Rancid-Tomatillos/static/media/tomatillo.f89c729e.png"
+  }
+]);
